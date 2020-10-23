4ebe2796736720756036ef536a473376ebaa36bf
7carlosz <7carlosz@gmail.com>
2020-10-23 09:31:37 -0300

    se agrega metodo search cust

--- core/core-service.go
+++ core/core-service.go
@@ -44,6 +44,29 @@ func CoreReadBySearchLikeCustom(query string, req interface{}, entity interface{
 
 }
 
+func CoreReadBySearchCustom(query string, req interface{}, entity interface{}, ctx context.Context, c *sql.Conn, dateValidate, hourValidate, dateHourValidate string, tabla string) ([]interface{}, error) {
+
+	where, vals, order, limitOrder := utils.BuildWherePageable(req, false, false)
+	_, selectArray := utils.BuildSelect(entity)
+
+	query = query + where + " " + order + " " + limitOrder
+	log.Println(query)
+	rows, err := c.QueryContext(ctx, query,
+		vals...,
+	)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to select -> "+err.Error())
+	}
+	defer rows.Close()
+
+	list := TraducirRespuestaListCore(entity, rows, selectArray, dateValidate, hourValidate, dateHourValidate)
+	if len(list) < 1 {
+		return nil, status.Error(codes.NotFound, "Recurso no encontrado")
+	}
+	return list, nil
+
+}
+
 func CoreReadCustom(query string, req interface{}, entity interface{}, ctx context.Context, c *sql.Conn, dateValidate, hourValidate, dateHourValidate string, tabla string) ([]interface{}, error) {
 
 	_, vals := utils.BuildWhere(req)
