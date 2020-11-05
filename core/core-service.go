ffc85eb7879135a45a2440ebb3b6d99f02559ebb
7carlosz <7carlosz@gmail.com>
2020-11-04 21:32:27 -0300

    se agrega like or

--- core/core-service.go
+++ core/core-service.go
@@ -43,6 +43,27 @@ func CoreReadBySearchLikeCustom(query string, req interface{}, entity interface{
 	return list, nil
 
 }
+func CoreReadBySearchLikeOrCustom(query string, req interface{}, entity interface{}, ctx context.Context, c *sql.Conn, dateValidate, hourValidate, dateHourValidate string, tabla string) ([]interface{}, error) {
+
+	where, vals, order, limitOrder := utils.BuildWherePageable(req, true, true)
+	_, selectArray := utils.BuildSelect(entity)
+
+	query = query + where + " " + order + " " + limitOrder
+	rows, err := c.QueryContext(ctx, query,
+		vals...,
+	)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed s to select -> "+err.Error())
+	}
+	defer rows.Close()
+
+	list := TraducirRespuestaListCore(entity, rows, selectArray, dateValidate, hourValidate, dateHourValidate)
+	if len(list) < 1 {
+		return nil, status.Error(codes.NotFound, "Recurso no encontrado")
+	}
+	return list, nil
+
+}
 
 func CoreReadBySearchGroupByCustom(query string, groupBy string, req interface{}, entity interface{}, ctx context.Context, c *sql.Conn, dateValidate, hourValidate, dateHourValidate string, tabla string) ([]interface{}, error) {
 
